e68f0a6b4f52f78ba1aa774edffa02553ee60e4b
D3KRISH <83129000+D3KRISH@users.noreply.github.com>
2022-08-06 22:53:47 +0530

    Create is.js

---
+++ is.js
@@ -0,0 +1,23 @@
+// bad
+{
+  bruceBanner: {
+    color: 'pink',
+    transition: 'color 10s',
+  },
+
+  bruceBannerTheHulk: {
+    color: 'green',
+  },
+}
+
+// good
+{
+  bruceBanner: {
+    color: 'pink',
+    transition: 'color 10s',
+  },
+
+  bruceBanner_theHulk: {
+    color: 'green',
+  },
+}
